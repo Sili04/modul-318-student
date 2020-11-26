b0aaa1ca077fd28ff324344ae4399d73bac5c76a
Sili04 <Silvan_Heini@sluz.ch>
2020-11-26 11:00:01 +0100

    Created new win forms project

---
+++ ST-Application/VerbindungenSuchenForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ST_Application
+{
+  partial class VerbindungenSuchenForm
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // VerbindungenSuchenForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Name = "VerbindungenSuchenForm";
+      this.Text = "ÖV finder - Verbindungen suchen";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
